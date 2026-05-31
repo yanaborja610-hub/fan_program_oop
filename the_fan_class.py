e6e2c0a4a6a844edb77663dd99303deebdcc3245
yanab <yanaborja610@gmail.com>
2026-05-31 15:39:58 +0800

    Added the setter methods for all four data fields.

--- the_fan_class.py
+++ the_fan_class.py
@@ -9,6 +9,7 @@ class Fan:
         self.__color = color
         self.__on = on
 
+    # getters
     def get_speed(self):
         return self.__speed
 
@@ -20,3 +21,22 @@ class Fan:
 
     def get_power(self):
         return self.__on
+
+    # setters
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def set_color(self, color):
+        self.__color = color
+
+    def set_power(self, power):
+        self.__on = power
+
+    def turn_on(self):
+        self.__on = True
+
+    def turn_off(self):
+        self.__on = False
