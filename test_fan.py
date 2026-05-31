da0fb8ec1a8cfb805bf87881b3b05a6eff11da47
yanab <yanaborja610@gmail.com>
2026-05-31 16:05:14 +0800

    Displaying the fan properties of the first and second object (fan)

--- test_fan.py
+++ test_fan.py
@@ -2,3 +2,9 @@ from the_fan_class import Fan
 
 fan_1 = Fan(Fan.fast, 10, "Yellow", True)
 fan_2 = Fan(Fan.medium, 5, "Blue", False)
+
+print("FIRST FAN")
+fan_1.display()
+
+print("SECOND FAN")
+fan_2.display()
