d61e03457291600fc39e989fd8f3b213358d5282
yanab <yanaborja610@gmail.com>
2026-05-31 15:23:03 +0800

    Initialize the created class and added instance variables inside it such as speed, radius, color, and on

--- the_fan_class.py
+++ the_fan_class.py
@@ -2,3 +2,9 @@ class Fan:
     slow = 1
     medium = 2
     fast = 3
+
+    def __init__(self, speed, radius, color, on):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
